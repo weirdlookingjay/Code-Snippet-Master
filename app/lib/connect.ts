dc1fb2684f8cba9662e075fae563721f33ac756f
Joe Clark <weirdlookingjay@live.com>
2024-07-08 14:49:04 -0400

    updated connect.ts db env

--- app/lib/connect.ts
+++ app/lib/connect.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connect(): Promise<void> {
   try {
-    await mongoose.connect(process.env.MONGO_URL as string);
+    await mongoose.connect(process.env.MONGODB_URL as string);
     console.log("db connected...");
   } catch (error) {
     console.error(error);
